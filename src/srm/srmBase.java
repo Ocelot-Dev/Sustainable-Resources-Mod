1365a9241f5689f8d10b29f5c641492b3018ac66
William (Cody) Jack Young <cody_white_tiger@live.com>
2013-04-06 20:25:45 -0500

    cleaned code.

--- src/srm/srmBase.java
+++ src/srm/srmBase.java
@@ -5,9 +5,11 @@ import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = "Sustainable Resources Mod")
+
 public class srmBase
 {
 	@Init
+	
 	public void init(FMLInitializationEvent event)
 	{
 		System.out.println("Initilizing Sustainable Resources Mod  (SMR)");
